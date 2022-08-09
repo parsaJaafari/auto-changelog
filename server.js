3a4e071a04eddaa70053905d193098fa2926545e
Parsa Jaafari <parsaj79@gmail.com>
2022-08-09 14:02:54 +0430

    refactor: add a route handler

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.end("hello from the server");
+});
+
 app.listen(3000, () => console.log("listening on port 3000"));

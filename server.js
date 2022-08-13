a9a88f720c5657f2a49be6d9a311027847db36ca
Parsa Jaafari <parsaj79@gmail.com>
2022-08-13 13:51:25 +0430

    feat: add another port running

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.end("hello from the server");
 });
 
-app.listen(3000, () => console.log("listening on port 3000"));
+app.listen(4000, () => console.log("listening on port 3000"));
 
 console.log("new log on server");
 
